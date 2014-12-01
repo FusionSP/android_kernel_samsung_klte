a023dd1deecce2a8a5c58598ac1d4858cf8eee81
Dragon <dragon7780@hotmail.nl>
2014-12-01 12:28:15 +0100

    Remove intellidemand cpu governor
    The intellidemand cpu governor is outdated. Also due the implementation of
    intelliminmax cpu governor this commit will fix the kernels build.
    
    Signed-off-by: Dragon <dragon7780@hotmail.nl>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -449,9 +449,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
-extern struct cpufreq_governor cpufreq_gov_intellidemand;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_INTELLIDEMAND)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_DANCEDANCE)
